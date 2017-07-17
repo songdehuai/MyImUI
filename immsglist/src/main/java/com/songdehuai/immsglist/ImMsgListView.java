a1435c0bfa7454a0aa923d95ff80171da38d207a
YangweiHuayiWorkPC <916692273@qq.com>
2017-07-17 09:17:29 +0800

    自动构建出来的变化

--- immsglist/src/main/java/com/songdehuai/immsglist/ImMsgListView.java
+++ immsglist/src/main/java/com/songdehuai/immsglist/ImMsgListView.java
@@ -2,6 +2,7 @@ package com.songdehuai.immsglist;
 
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -18,5 +19,10 @@ public class ImMsgListView extends View {
     public ImMsgListView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
-    
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        
+    }
 }
